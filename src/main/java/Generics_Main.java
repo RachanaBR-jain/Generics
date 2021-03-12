f536fb9330d17cae28aea1120cc163a2c83c7916
RachanaBR-jain <br19rachanajain@gmail.com>
2021-03-12 07:57:27 +0530

    refactered to generic class to find the max num

--- src/main/java/Generics_Main.java
+++ src/main/java/Generics_Main.java
@@ -1,13 +1,25 @@
-public class Generics_Main {
+public class Generics_Main <E extends Comparable<E>>{
+        E value1, value2, value3;
 
-    public <E extends Comparable<E>> E findMax_Generics(E value1, E value2, E value3) {
-        E max = value1;
-        if (value2.compareTo(max) > 0) {
-            max = value2;
+        public Generics_Main(E value1, E value2, E value3) {
+            this.value1 = value1;
+            this.value2 = value2;
+            this.value3 = value3;
         }
-        if (value3.compareTo(max) > 0) {
-            max = value3;
+
+        public E maxFind() {
+            return Generics_Main.findMax_Generics(value1, value2, value3);
         }
-        return max;
+
+        public static <E extends Comparable<E>> E findMax_Generics(E value1, E value2, E value3) {
+            E max = value1;
+            if (value2.compareTo(max) > 0) {
+                max = value2;
+            }
+            if (value3.compareTo(max) > 0) {
+                max = value3;
+            }
+            return max;
+        }
     }
-}
+
--- src/test/java/Generics_Testing.java
+++ src/test/java/Generics_Testing.java
@@ -6,43 +6,49 @@ public class Generics_Testing {
 
         Generics_Main maxOperation;
 
-        @BeforeEach
-        public void setup() {
-            maxOperation = new Generics_Main();
-        }
-
         @Test
         public void givenThreeIntegers_AtFirstPosition_ShouldReturnMaximumValue() {
+            maxOperation = new Generics_Main(10,7,1);
             Integer maxInteger = maxOperation.findMax_Generics(10, 7, 1);
             Assertions.assertEquals(Integer.valueOf(10), maxInteger);
         }
 
         @Test
         public void givenThreeIntegers_AtSecondPosition_ShouldReturnMaximumValue() {
+            maxOperation = new Generics_Main(5,10,2);
+
             Integer maxInteger = maxOperation.findMax_Generics(5, 10, 2);
             Assertions.assertEquals(Integer.valueOf(10), maxInteger);
         }
 
         @Test
         public void givenThreeIntegers_AtThirdPosition_ShouldReturnMaximumValue() {
+            maxOperation = new Generics_Main(5,2,10);
+
             Integer maxInteger = maxOperation.findMax_Generics(5, 2, 10);
             Assertions.assertEquals(Integer.valueOf(10), maxInteger);
         }
 
         @Test
         public void givenThreeFloats_AtFirstPosition_ShouldReturnMaximumValue() {
+            maxOperation = new Generics_Main(10.33f, 1.2f, 5.7f);
+
             Float maxFloat = maxOperation.findMax_Generics(10.33f, 1.2f, 5.7f);
             Assertions.assertEquals(Float.valueOf(10.33f), maxFloat);
         }
 
         @Test
         public void givenThreeFloats_AtSecondPosition_ShouldReturnMaximumValue() {
+            maxOperation = new Generics_Main(1.2f, 10.33f, 5.8f);
+
             Float maxFloat = maxOperation.findMax_Generics(1.2f, 10.33f, 5.8f);
             Assertions.assertEquals(Float.valueOf(10.33f), maxFloat);
         }
 
         @Test
         public void givenThreeFloats_AtThirdPosition_ShouldReturnMaximumValue() {
+            maxOperation = new Generics_Main(5.5f, 3.2f, 10.33f);
+
             Float maxFloat = maxOperation.findMax_Generics(5.5f, 3.2f, 10.33f);
             Assertions.assertEquals(Float.valueOf(10.33f), maxFloat);
         }
@@ -50,18 +56,23 @@ public class Generics_Testing {
 
     @Test
     public void givenThreeString_AtFirstPosition_ShouldReturnMaximumValue() {
+        maxOperation = new Generics_Main("Orange", "Apple", "Banana");
+
         String maxString = maxOperation.findMax_Generics("Orange", "Apple", "Banana");
         Assertions.assertEquals("Orange", maxString);
     }
 
     @Test
     public void givenThreeString_AtSecondPosition_ShouldReturnMaximumValue() {
+        maxOperation = new Generics_Main("Apple", "Orange", "Banana");
         String maxString = maxOperation.findMax_Generics("Apple", "Orange", "Banana");
         Assertions.assertEquals("Orange", maxString);
     }
 
     @Test
     public void givenThreeString_AtThirdPosition_ShouldReturnMaximumValue() {
+        maxOperation = new Generics_Main("Apple", "Banana", "Orange");
+
         String maxString = maxOperation.findMax_Generics("Apple", "Banana", "Orange");
         Assertions.assertEquals("Orange", maxString);
     }
